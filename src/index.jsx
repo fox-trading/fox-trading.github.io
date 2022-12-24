da8ccf04b7787dcb83ebc9bb992bcf87ebdf94c0
Mansur Mabo <mansurmabo@gmail.com>
2022-12-24 16:32:20 +0600

    add favicons

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./Helpers/ScrollToTop";
 
@@ -21,4 +20,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
